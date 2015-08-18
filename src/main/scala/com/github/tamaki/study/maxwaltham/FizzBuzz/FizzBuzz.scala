a4ea8326307e8324568ef332f5b50f432bb5891a
Kota Saito <max.k.saito@gmail.com>
2015-08-18 19:05:09 +0900

    FizzBuzzの別解

--- src/main/scala/com/github/tamaki/study/maxwaltham/FizzBuzz/FizzBuzz.scala
+++ src/main/scala/com/github/tamaki/study/maxwaltham/FizzBuzz/FizzBuzz.scala
@@ -16,5 +16,12 @@ object FizzBuzz {
       case x => println(x)
     })
 
+
+    val fb = (1 to 100).map(x => (x%3, x%5) match{
+      case (0,0) => "FizzBuzz"
+      case (0,_) => "Fizz"
+      case (_, 0) => "Buzz"
+      case _ => x.toString
+    }).foreach(println(_))
   }
 }
