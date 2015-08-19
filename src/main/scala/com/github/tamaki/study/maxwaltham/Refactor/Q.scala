36be55e2d58af4856f5a8c540c46507c1dfc64b2
Kota Saito <max.k.saito@gmail.com>
2015-08-19 10:55:55 +0900

    q3まで

--- src/main/scala/com/github/tamaki/study/maxwaltham/Refactor/Q.scala
+++ src/main/scala/com/github/tamaki/study/maxwaltham/Refactor/Q.scala
@@ -3,18 +3,75 @@ package com.github.tamaki.study.maxwaltham.Refactor
 /**
  * Created by kota.saito on 2015/08/19.
  */
-class Q {
+object Q {
 
-  def findByName(name: String) = ""
+  def main(args: Array[String]) = {
+    q1(Some("kota"))
+    q1(None)
+    q3(Some(" kota "))
+    q3(Some("  "))
+  }
 
+  def findByName(name: String) = "id:" + name
+
+  /**
+   * Optionにmap
+   * @param nameOpt
+   */
   def q1(nameOpt: Option[String]) = {
     // bad
-    if(nameOpt.isDefined){
+    if (nameOpt.isDefined) {
       val name = nameOpt.get
       val id = findByName(name)
+      println(id)
     }
-    // good?
-    findByName(nameOpt.getOrElse(""))
 
+    // good
+    println(nameOpt.map(findByName))
   }
+
+  /**
+   * Optionにmatch
+   * @param nameOpt
+   */
+  def q2(nameOpt: Option[String]) = {
+    // bad
+    val a = nameOpt.isDefined match {
+      case true =>
+        None
+      case false =>
+        // do something
+        Some("x")
+    }
+
+    // good
+    val b = nameOpt match {
+      case None =>
+        ""
+      case Some(x) =>
+        // do something
+        x
+    }
+  }
+
+  /**
+   * OptionにwithFilter、foreach
+   * @param nameOpt
+   */
+  def q3(nameOpt: Option[String]) = {
+    // bad
+    nameOpt.foreach { name =>
+      if (name.trim.nonEmpty) {
+        println(name)
+      }
+    }
+
+    // good
+    nameOpt.withFilter(_.trim.nonEmpty).foreach {
+      println(_)
+    }
+  }
+
+  def q4 = "TODO"
+
 }
