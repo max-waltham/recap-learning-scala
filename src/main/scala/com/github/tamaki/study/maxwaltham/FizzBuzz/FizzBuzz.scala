ae67efe006d2e10c14ffe7ca2544bfc8b90813ab
Kota Saito <max.k.saito@gmail.com>
2015-08-26 19:31:41 +0900

    FizzBuzzの別解その３

--- src/main/scala/com/github/tamaki/study/maxwaltham/FizzBuzz/FizzBuzz.scala
+++ src/main/scala/com/github/tamaki/study/maxwaltham/FizzBuzz/FizzBuzz.scala
@@ -1,5 +1,7 @@
 package com.github.tamaki.study.kota.saito.FizzBuzz
 
+import scala.annotation.tailrec
+
 /**
  * Created by tamaki on 2015/02/08.
  */
@@ -17,13 +19,32 @@ object FizzBuzz {
     })
 
 
-    val fb = (1 to 100).map(x => (x%3, x%5) match{
-      case (0,0) => "FizzBuzz"
-      case (0,_) => "Fizz"
+    val fb = (1 to 100).map(x => (x % 3, x % 5) match {
+      case (0, 0) => "FizzBuzz"
+      case (0, _) => "Fizz"
       case (_, 0) => "Buzz"
       case _ => x.toString
     }) foreach println
 
-//    fizzBuzz(1000)
+    // fizzBuzz(1000)
+    def fizzBuzz(len: Int): String = {
+
+      @tailrec
+      def go(i: Int, acc: Seq[String]): Seq[String] =
+        if (i > len)
+          acc
+        else
+          (i % 3, i % 5) match {
+            case (0, 0) => go(i + 1, acc :+ "FizzBuzz")
+            case (0, _) => go(i + 1, acc :+ "Fizz")
+            case (_, 0) => go(i + 1, acc :+ "Buzz")
+            case _ => go(i + 1, acc :+ i.toString)
+          }
+
+      go(1, Nil).mkString(",")
+    }
+
+    println(fizzBuzz(100))
+
   }
 }
